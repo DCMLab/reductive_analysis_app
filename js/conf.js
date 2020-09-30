2b82973b79640a1c08adaf47890631f1492d67df
Petter Ericson <petter.ericson@epfl.ch>
2020-09-30 17:09:23 +0200

    Add basic configuration file

---
+++ js/conf.js
@@ -0,0 +1,36 @@
+var type_keys = {
+  "r": "repeat",
+  "p": "passing",
+  "n": "neighbour",
+  "i": "harmonic",
+  "a": "arpeggio",
+  "u": "urlinie",
+  "b": "bassbrechung"
+}
+
+var shades_array = d3.schemeTableau10;
+
+var type_shades = {
+  "repeat" : shades_array[0],
+  "prolongation" :shades_array[0],
+  "passing" :shades_array[6],
+  "neighbour" : shades_array[2],
+  "harmonic" :shades_array[3],
+  "arpeggio" : shades_array[4],
+  "arp" : shades_array[4],
+  "urlinie" : shades_array[5],
+  "bassbrechung" : shades_array[5],
+  "bassbrech" : shades_array[5],
+};
+
+var button_shades = {
+"repeathyperedgebutton" :shades_array[0],
+"passinghyperedgebutton": shades_array[6],
+"neighbourhyperedgebutton": shades_array[2],
+"harmonichyperedgebutton": shades_array[3],
+"arpeggiohyperedgebutton": shades_array[4],
+"urliniehyperedgebutton": shades_array[5],
+"bassbrechunghyperedgebutton" : shades_array[5],
+};
+
+

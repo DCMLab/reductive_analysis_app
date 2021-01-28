2939aa060a29b239fa39afa1a6a88cf9137fd06b
yrammos <y@rammos.co>
2021-01-28 19:19:26 +0200

    Refactor DOM loader to use async/wait.

--- index.test.js
+++ index.test.js
@@ -2,17 +2,25 @@ const JSDOM = require('jsdom').JSDOM
 
 let dom
 
-describe('index.html', () => {
-  beforeAll(() => {
+describe('load DOM', () => {
+
+  beforeAll(async () => {
+
     // The web app uses html-embeded <scripts> therefore invoking
-    // `beforeEach()` is necessary in order to initialize the test runner.
-    return JSDOM.fromFile("index.html", {
+    // `beforeAll()` is necessary in order to initialize the test runner.
+    dom = await JSDOM.fromFile("index.html", {
       runScripts: 'dangerously',   // load inline <scripts>
-      resources: "usable"   // load external resources
+      resources: "usable",   // load external resources
+      contentType: "text/html",
+      includeNodeLocations: true
     })
-    .then(d => {
-      dom = d
-    });
+    await new Promise(resolve => {
+      console.log('[JSDOM] DOM loaded.')
+      // The following line is crucial and took a lifetime to figure out.
+      // It is possibly necessary due to Jest bug to be fixed in Jest 27.
+      // See for instance https://github.com/facebook/jest/issues/1256
+      // Long story short, `beforeAll` does not handle asynch steps well(!)
+      dom.window.addEventListener("load", resolve) 
   })
 
   // Here is a dummy test to confirm that the testing framework is in place.

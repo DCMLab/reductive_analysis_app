498a4fe10436e0c6db8539d5e707b032ca281c8e
yrammos <y@rammos.co>
2021-01-29 10:43:47 +0200

    Improve commentary.

--- index.test.js
+++ index.test.js
@@ -6,8 +6,8 @@ describe('load DOM', () => {
 
   beforeAll(async () => {
 
-    // The web app uses html-embeded <scripts> therefore invoking
-    // `beforeAll()` is necessary in order to initialize the test runner.
+    // The web app uses html-embeded <scripts>, therefore 
+    // `beforeAll()` is necessary to initialize the test runner.
     dom = await JSDOM.fromFile("index.html", {
       runScripts: 'dangerously',   // load inline <scripts>
       resources: "usable",   // load external resources
@@ -17,15 +17,16 @@ describe('load DOM', () => {
     await new Promise(resolve => {
       console.log('[JSDOM] DOM loaded.')
       // The following line is crucial and took a lifetime to figure out.
-      // It is possibly necessary due to Jest bug to be fixed in Jest 27.
+      // It is possibly necessary due to a Jest bug to be fixed in Jest 27.
       // See for instance https://github.com/facebook/jest/issues/1256
-      // Long story short, `beforeAll` does not handle asynch steps well(!)
+      // Long story short, `beforeAll` does not handle async calls as well
+      // as it is supposed to be.
       dom.window.addEventListener("load", resolve) 
     })
 
   })
 
-  // Allow Jest hooks more than the default 5000ms to complete, or Jest might crash.
+  // Allow Jest hooks longer than 5000ms to complete, or Jest might crash.
   jest.setTimeout(3 * 60 * 1000)
 
   // Run a trivial test to confirm that the testing framework is in place.

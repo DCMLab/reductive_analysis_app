8ebc2e7a107c8d5a579a4adc3a2fe7bee8372299
yrammos <y@rammos.co>
2021-02-13 00:47:25 +0200

    Re-indent index.test.js according to project style.

--- index.test.js
+++ index.test.js
@@ -7,43 +7,43 @@ let $
 
 describe('load DOM', () => {
 
-	beforeAll(async () => {
+  beforeAll(async () => {
 
-		// The web app uses html-embeded <scripts>, therefore 
-		// `beforeAll()` is necessary to initialize the test runner.
-		dom = await JSDOM.fromFile("index.html", {
-			runScripts: 'dangerously',	 // load inline <scripts>
-			resources: "usable",	 // load external resources
-			contentType: "text/html",
-			includeNodeLocations: true
-		})
+    // The web app uses html-embeded <scripts>, therefore 
+    // `beforeAll()` is necessary to initialize the test runner.
+    dom = await JSDOM.fromFile("index.html", {
+      runScripts: 'dangerously',   // load inline <scripts>
+      resources: "usable",   // load external resources
+      contentType: "text/html",
+      includeNodeLocations: true
+    })
 
-		await new Promise(resolve => {
-			console.log('[JSDOM] DOM loaded.')
-			// The following line is crucial and took a lifetime to figure out.
-			// It is possibly necessary due to a Jest bug to be fixed in Jest 27.
-			// See for instance https://github.com/facebook/jest/issues/1256
-			// Long story short, `beforeAll` does not handle async calls as well
-			// as it is supposed to be.
-			dom.window.addEventListener("load", resolve) 
-		})
+    await new Promise(resolve => {
+      console.log('[JSDOM] DOM loaded.')
+      // The following line is crucial and took a lifetime to figure out.
+      // It is possibly necessary due to a Jest bug to be fixed in Jest 27.
+      // See for instance https://github.com/facebook/jest/issues/1256
+      // Long story short, `beforeAll` does not handle async calls as well
+      // as it is supposed to be.
+      dom.window.addEventListener("load", resolve) 
+    })
 
-		// Create helpers for quering the DOM in tests.
-		window = dom.window
-		document = window.document
-		$ = window.$
+    // Create helpers for quering the DOM in tests.
+    window = dom.window
+    document = window.document
+    $ = window.$
 
-	})
+  })
 
-	// Allow Jest hooks longer than 5000ms to complete, or Jest might crash.
-	jest.setTimeout(3 * 60 * 1000)
+  // Allow Jest hooks longer than 5000ms to complete, or Jest might crash.
+  jest.setTimeout(3 * 60 * 1000)
 
-	// Run a trivial test to confirm that the testing framework is in place.
-	it('displays the Primaries and Secondaries labels in the layout', function() {
-		expect(
-			$('#selected_things').html()
-			.match(/Primaries:.*<br>Secondaries:.*/g)
-		).toBeTruthy();
-	});
+  // Run a trivial test to confirm that the testing framework is in place.
+  it('displays the Primaries and Secondaries labels in the layout', function() {
+    expect(
+      $('#selected_things').html()
+      .match(/Primaries:.*<br>Secondaries:.*/g)
+    ).toBeTruthy();
+  });
 
 });

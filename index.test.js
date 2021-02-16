a3627194f83ee41399c28b8671de53039c9c6369
Petter Ericson <petter.ericson@epfl.ch>
2021-02-16 12:20:05 +0100

    Modified tests to include resolves async modifier

--- index.test.js
+++ index.test.js
@@ -1,17 +1,23 @@
 require('expect-puppeteer');
 
 describe('reductive_analysis_test_suite', () => {
-  jest.setTimeout(30000);
 
-  beforeAll(async () => {
-    await new Promise(resolve => {
-      document.addEventListener("load", resolve);
-      console.log("DOM fully loaded and parsed");
-    })
+  beforeAll(async () => { await page.goto("http://localhost:8000");
+      console.log("DOM fully loaded and parsed?");
   });
 
   it('should run a rudimentary test on static HTML to confirm Jest works', async function() {
-    await expect(page.title()).toMatch('DCML.*/s', {timeout: 30000})
-    await expect(page).toClick('button', { text: 'Choose File' })
+    await expect(page.title()).resolves.toMatch(/DCML.*/s, {timeout: 30000})
   });
+
+  it('should have loaded the config.js and set up appropriate buttons', async function() {
+      // Check that e.g. the repeat button exists (with id
+      // "repeatrelationbutton"), is a button of the right class
+      // (relationbutton), and has the right colour
+  });
+
+  it('should load the example MEI', async function() {
+      // Use puppeteer to load the MEI into the input with id "fileupload"
+  });
+
 });

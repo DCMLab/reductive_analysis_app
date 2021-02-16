3df6d5131dc481a6ff4f6a4c90bf0969dedd7367
yrammos <y@rammos.co>
2021-02-16 03:31:12 +0200

    Handle the asynchronicity of DOM loading.

--- index.test.js
+++ index.test.js
@@ -1,6 +1,15 @@
 require('expect-puppeteer');
 
 describe('reductive_analysis_test_suite', () => {
+  jest.setTimeout(30000);
+
+  beforeAll(async () => {
+    await new Promise(resolve => {
+      document.addEventListener("load", resolve);
+      console.log("DOM fully loaded and parsed");
+    })
+  });
+
   it('should run a rudimentary test on static HTML to confirm Jest works', async function() {
     await expect(page.title()).toMatch('DCML.*/s', {timeout: 30000})
     await expect(page).toClick('button', { text: 'Choose File' })

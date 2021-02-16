b4c3c20a5a6e2745c3c6af6835491fce4ce6bb2f
yrammos <y@rammos.co>
2021-02-16 03:30:58 +0200

    Fix syntax error.

--- index.test.js
+++ index.test.js
@@ -2,6 +2,7 @@ require('expect-puppeteer');
 
 describe('reductive_analysis_test_suite', () => {
   it('should run a rudimentary test on static HTML to confirm Jest works', async function() {
-    await setTimeout(() => expect(page.title()).toMatch('DCML.*/s'), 5000)
+    await expect(page.title()).toMatch('DCML.*/s', {timeout: 30000})
+    await expect(page).toClick('button', { text: 'Choose File' })
   });
 });

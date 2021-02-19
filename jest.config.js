22716b11b76c7275c42eb2c58710650f56b0ab2b
yrammos <y@rammos.co>
2021-02-19 13:52:02 +0200

    Add dependency on jest-extended, which provides convenient test assertions.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     preset: "jest-puppeteer",
-    setupFilesAfterEnv: ["expect-puppeteer"],
+    setupFilesAfterEnv: ["expect-puppeteer", "jest-extended"],
     globals: {
       launchTimeout: 60 * 10 * 1000   // in ms
     },

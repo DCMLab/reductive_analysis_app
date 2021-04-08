d6ea0bf3ba92c9003a672602b67404a7c3f096d3
Petter Ericson <petter.ericson@epfl.ch>
2021-04-08 14:35:39 +0200

    Some rearranging, mostly documentation.

--- js/draw.js
+++ js/draw.js
@@ -1,7 +1,14 @@
+// Given a draw context and a graph node representing a relation, draw the
+// relation in the draw context.
 function draw_relation(draw_context, mei_graph, g_elem) {
   var added = [];
+  // Where are we drawing, and with what prefix?
   var svg_elem = draw_context.svg_elem;
   var id_prefix = draw_context.id_prefix;
+  // ID and type
+  var id = id_prefix + g_elem.getAttribute("xml:id");
+  var type = relation_type(g_elem);
+  // What are the primary and secondary notes in the draw context?
   var primaries = relation_primaries(mei_graph,g_elem).map(
       (e) => document.getElementById(id_in_svg(draw_context,node_to_note_id(e)))
     );
@@ -15,31 +22,36 @@ function draw_relation(draw_context, mei_graph, g_elem) {
       var p1= note_coords(a);
       var p2= note_coords(b);
       return (p1[0] - p2[0]) ? (p1[0] - p2[0]) : (p1[1] - p2[1]);
-    });
+  });
+  // TODO: Should be able to draw relations that are not complete
   if(!notes[0]) {
     console.log("Note missing, relation not drawn");
     return null;
   }
 
-  var id = id_prefix + g_elem.getAttribute("xml:id");
-  var type = relation_type(g_elem);
-
+  // TODO: Other ways to draw the relations - retain as a single tree with
+  // ID and type. TODO: Use classlist also for types, as in type:<type> or
+  // similar
   var elem = roundedHull(notes.map(note_coords));
   elem.setAttribute("id",id);
   if(id_prefix != "")
     elem.setAttribute("oldid",g_elem.getAttribute("xml:id"));
   elem.classList.add("relation");
   elem.setAttribute("type",type);
+
+  // Are we running with type-specific shades?
+  if(shades)
+    toggle_shade(elem);
+
+  // Relations can be scrolled
   elem.onwheel = (ev) => {
     var elem1 = ev.target;
     flip_to_bg(elem1);
     elem.onmouseout();
     return false;
   };
-  if(shades)
-    toggle_shade(elem);
-  add_to_svg_bg(svg_elem,elem);
-  added.push(elem);
+
+  // Decorate with onclick and onmouseover handlers
   elem.onclick = function(ev) {toggle_selected(elem,ev.shiftKey);};
   elem.onmouseover = function (ev) {
     primaries.forEach(  (item) => { item.classList.add("extrahover"); });
@@ -52,49 +64,64 @@ function draw_relation(draw_context, mei_graph, g_elem) {
   //TODO: Set up more onhover stuff for The Same Relation
   //Elsewhere - but perhaps that's a separate thing?
 
+  // Add it to the SVG
+  add_to_svg_bg(svg_elem,elem);
+  // Remember what we're adding
+  added.push(elem);
   return added;
 
 }
 
+
+// Essentially the same procedure as above, but for metarelations
 function draw_metarelation(draw_context, mei_graph, g_elem) {
   var added = [];
-  var mei = draw_context.mei;
+  // Draw target, prefix, ID and type
   var svg_elem = draw_context.svg_elem;
   var id_prefix = draw_context.id_prefix;
   var id = id_prefix + g_elem.getAttribute("xml:id");
   var type = relation_type(g_elem);
+  // Get the targets - we don't differentiate primaries and secondaries in 
+  // this drawing style.
   var targets = relation_allnodes(mei_graph, g_elem).map(
       (e) => document.getElementById(draw_context.id_prefix+get_id(e)));
+  // TODO should be possible to draw partial metarelations
   if(targets.indexOf(null) != -1){
     console.log("Missing relation, not drawing metarelation");
     return [];
   }
-
+  
+  // Where are our targets
   var coords = targets.map(get_metarelation_target);
+  // What's midpoint above them?
   var x = average(coords.map((e) => e[0]));
-  // Above
+  // Above the system, and also above the relations
   var y = targets.concat([svg_elem.getElementsByClassName("system")[0]]).map((b) => b.getBBox().y).sort((a,b) => a > b)[0] - 500;
 
   coords.push([x,y]);
+  // We make a group
   var g_elem = g(svg_elem);
-  var elem = roundedHull(coords);
   g_elem.setAttribute("id",id);
   g_elem.classList.add("metarelation");
+  //TODO: Use classlist for types
   g_elem.setAttribute("type",type);
-  g_elem.onwheel = (e) => {
-    var elem1 = e.target;
-    var paren = elem1.parentElement;
-    paren.removeChild(elem1);
-    paren.insertBefore(elem1,paren.children[0]);
-    return false;
-  };
+  // Draw the metarelation as a circle connected with lines to each of its
+  // targets
+  g_elem.appendChild(circle([x,y],200));
   coords.forEach((crds) => { var line_elem = line([x,y],crds);
       g_elem.appendChild(line_elem);});
-  g_elem.appendChild(circle([x,y],200));
+  // Type-dependent shades
   if(shades)
     toggle_shade(elem);
-  add_to_svg_bg(svg_elem,g_elem);
-  added.push(g_elem);
+  // We can scroll among metarelations as well
+  g_elem.onwheel = (e) => {
+    var elem1 = e.target;
+    flip_to_bg(elem1);
+    elem.onmouseout();
+    return false;
+  };
+
+  //Decorate with onclick and onmouseover handlers
   g_elem.onclick = function(ev) {toggle_selected(g_elem,ev.shiftKey);};
   g_elem.onmouseover = function (ev) {
     targets.forEach((item) => { item.classList.add("relationhover"); });
@@ -102,5 +129,13 @@ function draw_metarelation(draw_context, mei_graph, g_elem) {
   g_elem.onmouseout = function (ev) {
     targets.forEach((item) => { item.classList.remove("relationhover"); });
   }
+
+  //TODO: Set up more onhover stuff for The Same Relation
+  //Elsewhere - but perhaps that's a separate thing?
+
+  // Add it to the SVG
+  add_to_svg_bg(svg_elem,g_elem);
+  // Remember what we're adding
+  added.push(g_elem);
   return added;
 }

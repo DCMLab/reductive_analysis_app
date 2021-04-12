31e3c56318523a06a5872e827a2a59a9acc96162
Petter Ericson <petter.ericson@epfl.ch>
2021-04-12 12:12:37 +0200

    Move various UI functions to ui.js

--- js/main.js
+++ js/main.js
@@ -60,19 +60,6 @@ var placing_note="";
 var mouseX;
 var mouseY;
 
-window.onmousemove = (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-  // Not sure if this is the best way...
-  var elem = document.elementFromPoint(mouseX, mouseY);
-  var dc = draw_contexts.find((dc) => dc.view_elem.contains(elem));
-  if(dc)
-    current_draw_context = dc;
-  if(placing_note!=""){
-    update_placing_note();
-  }
-}
-
 // Prevent unsaved data loss by warning user before browser unload events (reload, close).
 // Attempting to do this in compliant fashion (https://html.spec.whatwg.org/#prompt-to-unload-a-document).
 window.addEventListener("beforeunload", function (e) {
@@ -91,23 +78,6 @@ $(document).ready(function() {
   $("#player").midiPlayer({ color: "grey", width: 250 });
   $("#selected_things").hide();
 });
-
-// If we're selecting relations, we may want to change them.
-function toggle_he_selected(selecting) {
-  console.debug("Using globals: document for changing button texts/visibility");
-
-  Array.from(document.getElementsByClassName("relationbutton")).forEach((button) => {
-    var val = button.getAttribute("value");
-    if(selecting)
-      button.setAttribute("value",val.replace("Add","Set to"));
-    else
-      button.setAttribute("value",val.replace("Set to","Add"));
-  });
-  if(selecting)
-    document.getElementById("meta_buttons").classList.remove("none");
-  else
-    document.getElementById("meta_buttons").classList.add("none");
-}
 
 // Toggle if a thing (for now: note or relation) is selected or not.
 function toggle_selected(item,extra) { 
@@ -163,80 +133,6 @@ function toggle_selected(item,extra) {
   }
 
   update_text();
-}
-
-
-function update_text(){
-  var primaries, secondaries;
-    primaries = to_text(draw_contexts,mei_graph,extraselected);
-    secondaries = to_text(draw_contexts,mei_graph,selected);
-  if (primaries || secondaries) {
-    $("#selected_things").show();
-    $("#selected_things").html("<span class='selected_primaries'>Primaries: </span>"+primaries+"<br/><span class='selected_secondaries'>Secondaries: </span>"+secondaries);  
-  } else {
-    $("#selected_things").hide();
-  }
-}
-
-// Toggle showing things other than notes in the score
-function toggle_equalize() {
-  console.debug("Using globals: non_notes_hidden");
-  non_notes_hidden = !non_notes_hidden;
-  set_non_note_visibility(non_notes_hidden);
-}
-
-function set_non_note_visibility(hidden) {
-  console.debug("Using globals: document for element selection");
-  Array.from(document.getElementsByClassName("beam")).forEach((x) => {
-    Array.from(x.children).forEach((x) => { 
-      if(x.tagName == "polygon") { 
-        hidden ? x.classList.add("hidden") : x.classList.remove("hidden"); 
-      }
-    })
-  });
-  hide_classes.forEach((cl) => {
-    Array.from(document.getElementsByClassName(cl)).forEach((x) => { 
-      hidden ? x.classList.add("hidden") : x.classList.remove("hidden");
-    })
-  });
-}
-
-
-// Toggle the current relation having a type-dependent shade
-// or not
-function toggle_shade(he) {
-  console.debug("Using globals: shades, type_shades, meta_shades, type_synonym");
-  if(!shades && he.getAttribute("old_fill")){
-    he.setAttribute("fill",he.getAttribute("old_fill"));
-    he.removeAttribute("old_fill");
-  }else if (shades && type_shades[he.getAttribute("type")]){
-    he.setAttribute("old_fill", he.getAttribute("fill"));
-    he.setAttribute("fill",type_shades[he.getAttribute("type")]);
-  }else if (shades && meta_shades[he.getAttribute("type")]){
-    he.setAttribute("old_fill", he.getAttribute("fill"));
-    he.setAttribute("fill",meta_shades[he.getAttribute("type")]);
-  }else if (shades && type_synonym[he.getAttribute("type")]){
-    he.setAttribute("old_fill", he.getAttribute("fill"));
-    he.setAttribute("fill",type_shades[type_synonym[he.getAttribute("type")]]);
-  }
-}
-
-function toggle_button_shade(button) {
-  console.debug("Using globals: shades, button_shades");
-  if(shades) 
-    button.style.color=button_shades[button.getAttribute("id")];
-  else
-    button.style.color="";
-}
-
-// Toggle type-dependent shades for relations and buttons
-function toggle_shades() {
-  console.debug("Using globals: shades, document for element selection");
-  shades = !shades;
-  Array.from(document.getElementsByClassName("relation")).forEach(toggle_shade);
-  Array.from(document.getElementsByClassName("metarelation")).forEach(toggle_shade);
-  Array.from(document.getElementsByClassName("relationbutton")).forEach(toggle_button_shade);
-  Array.from(document.getElementsByClassName("metarelationbutton")).forEach(toggle_button_shade);
 }
 
 
@@ -384,66 +280,6 @@ function do_undo() {
       c.parentNode.insertBefore(c.children[0],c);
       c.parentNode.removeChild(c);
     }
-  }
-}
-
-function handle_keydown(ev) {
-  if(ev.key=="Control"){
-    start_placing_note();
-  }
-  // Global `.shift-pressed` class for pretty (meta-)relation styling on hover.
-  if (ev.key === "Shift")
-    $('#layers').addClass('shift-pressed')
-}
-
-function handle_keyup(ev) {
-  if(ev.key=="Control"){
-    stop_placing_note();
-  }
-  // Global `.shift-pressed` class for pretty (meta-)relation styling on hover.
-  if (ev.key === "Shift")
-    $('#layers').removeClass('shift-pressed')
-}
-
-function handle_click(ev) {
-  place_note();
-}
-
-
-// We have keyboard commands!
-function handle_keypress(ev) {
-  console.debug("Using globals: text_input, meta_keys, type_keys");
-  if(text_input)
-    return;
-  if (ev.key == "Enter"){
-    do_edges();
-  } else if (ev.key == "z") { // Scroll through relations
-    elem = document.elementFromPoint(mouseX, mouseY);
-    flip_to_bg(elem);
-    elem.onmouseout();
-  } else if (ev.key == "u") { // UNDO
-    do_undo();
-  } else if (ev.key == "r") { // Reduce relations
-    do_reduce();
-  } else if (ev.key == "s") { // Show/hide ties etc.
-    toggle_equalize();
-  } else if (ev.key == "h") { // Toggle type-dependent shades
-    toggle_shades();
-  } else if (ev.key == "+") { // Select same notes in the measure
-    select_samenote();
-    do_relation("repeat");
-  } else if (ev.key == "x") { // Select same notes in the measure
-    toggle_placing_note();
-  } else if (ev.key == "d") { // Deselect all.
-    do_deselect();
-  } else if (ev.key == "D") { // Delete relations.
-    delete_relations();
-  } else if (type_keys[ev.key]) { // Add a relation
-    do_relation(type_keys[ev.key]);
-  } else if (meta_keys[ev.key]) { // Add a relation
-    do_metarelation(meta_keys[ev.key]);
-  }else {
-    console.log(ev);
   }
 }
 
@@ -789,43 +625,5 @@ function rerender(draw_context) {
 }
 
 
-function texton() { text_input = true; }
-function textoff() { text_input = false; }
-function show_buttons() {
-  $("#load_save")[0].classList.remove("hidden");
-  $("#hidden_buttons")[0].classList.add("hidden");
-}
-function hide_buttons() {
-  $("#load_save")[0].classList.add("hidden");
-  $("#hidden_buttons")[0].classList.remove("hidden");
-}
-
-function zoom_in(draw_context) {
-  draw_context.zoom = draw_context.zoom * 1.1;
-  draw_context.svg_elem.children[0].style.transform="scale("+draw_context.zoom+")";
-}
-function zoom_out(draw_context) {
-  draw_context.zoom = draw_context.zoom * 0.90909090909090;
-  draw_context.svg_elem.children[0].style.transform="scale("+draw_context.zoom+")";
-}
-
-
-function do_deselect() {
-  selected.forEach((x) => toggle_selected(x));
-  extraselected.forEach((x) => toggle_selected(x,true));
-}
-
-function play_midi() {
-  $("#player").midiPlayer.play("data:audio/midi;base64,"+orig_midi);
-}
-
-function play_midi_reduction(draw_context=draw_contexts[0]) {
-  var mei2 = rerender_mei(true, draw_context);
-  var data2 = new XMLSerializer().serializeToString(mei2);
-  vrvToolkit.loadData(data2);
-  $("#player").midiPlayer.play("data:audio/midi;base64,"+vrvToolkit.renderToMIDI());
-  vrvToolkit.loadData(data);
-
-}
 
 console.log("Main webapp library is loaded");

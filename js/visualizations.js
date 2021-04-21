d13a47135968eb6f520c23c83035d1c00b9e1084
Petter Ericson <petter.ericson@epfl.ch>
2021-04-21 12:15:39 +0200

    Function for calculating initial reductive 'layers' from a set of relations.

--- js/visualizations.js
+++ js/visualizations.js
@@ -1,9 +1,22 @@
 
 
+
+
+
 // Returns a list of list of IDs
-function calc_hierarchy(nodes, relations) {
-
-
+function calc_hierarchy(notes, relations) {
+  var ret = [];
+  var rels = relations;
+  do{
+    var [removed_relations, removed_notes] = calc_reduce(mei_graph, 
+							 rels, 
+							 rels);
+    ret.push(removed_notes);
+    console.log(removed_notes, removed_relations);
+    notes = notes.filter((x) => !ll_removed_notes.includes(x))
+    rels = rels.filter((x) => !removed_relations.includes(x))
+  }while(removed_notes.length + removed_relations.length  > 0)
+  return ret;
 }
 
 

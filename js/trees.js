f5efa8f2046f46ab25e0b6fed4925ea029641e48
Petter Ericson <petter.ericson@epfl.ch>
2021-05-12 14:35:01 +0200

    Various changes to accomodate XML saving through add_tree and loading from an XML element through load_tree and find_x_tree.

--- js/trees.js
+++ js/trees.js
@@ -26,21 +26,13 @@ function gather_leaves(node) {
 }
 
 function calculate_x(node) {
-  if(node.children.length == 0)
-    return;
   node.children.forEach(calculate_x);
+  if(node.children.length == 0 || node.x != undefined)
+    return;
   node.x = average(node.children.map((n) => n.x));
 }
 
-function align_tree(tree,list, min_dist = -500) {
-  var leaves = gather_leaves(tree);
-  if(leaves.length != list.length){
-    console.log("Wrong length of list, expected "+leaves.length+" got "+list.length);
-    return;
-  }
-  for(i in leaves){
-    leaves[i].x = list[i];
-  }
+function align_tree(tree, min_dist = -500) {
   calculate_x(tree);
   calculate_initial_y(tree, min_dist);
   adjust_y(tree, tree.y, min_dist);
@@ -72,41 +64,125 @@ function draw_textbox(txt, padding=25) {
   txt.parentNode.insertBefore(text_rect,txt);
 }
 
+function get_subtree(json_tree) {
+  var elem, elems;
+
+  var lbl = mei.createElement("label");
+  lbl.append(json_tree.label);
+  console.log(json_tree.note_id);
+  if(json_tree.note_id){
+    var nt = mei.createElement("note");
+    nt.setAttribute("sameas","#"+json_tree.note_id);
+    lbl.appendChild(nt);
+  }
+
+  if(json_tree.children.length == 0){
+    elem = mei.createElement("eLeaf");
+  }else{
+    elem = mei.createElement("eTree");
+    elems = json_tree.children.map(get_subtree);
+  }
+
+  elem.appendChild(lbl);
+  if(elems)
+    elems.forEach((e) => elem.appendChild(e));
+
+  return elem;
+
+}
 
 
-function draw_tree(draw_context, input ="",baseline=0, min_dist = -1000) {
+function add_tree(json_tree) {
+  var pNode = mei_graph.parentNode;
+  var tree = get_subtree(json_tree);
+  // TODO: Add tree index/id/foo
+  pNode.appendChild(tree);
+}
+
+function load_subtree(elem){
+  var lbl = elem.children[0]; // The first child is the label
+  var obj = {
+    "label": lbl.textContent,
+  };
+  if(lbl.children.length != 0){
+    // Assume our structure - any label child is a note sameas
+    // TODO: fix, obviously
+    obj.note_id = lbl.querySelector("note").getAttribute("sameas").replace("#","");
+  }
+  var chlds = Array.from(elem.children);
+  chlds.shift(); //Get rid of the label
+  obj.children = chlds.map(load_subtree);
+  return obj;
+
+}
+
+
+function load_tree(elem) {
+  //TODO: any preprocessing or checks
+  return load_subtree(elem);
+  // Return an Object with label, children, and x-align node IDs
+}
+
+function find_x_tree(draw_context,tree){
+  if(tree.note_id)
+    tree.x = note_coords(get_by_id(document,id_in_svg(draw_context,tree.note_id)))[0];
+  tree.children.forEach((n) => find_x_tree(draw_context,n));
+}
+
+
+
+function draw_tree(draw_context, xmltree=undefined) {
   var svg_elem = draw_context.svg_elem;
   var id_prefix = draw_context.id_prefix;
+
   var svg_height = svg_elem.children[0].getAttribute("height");
   var svg_viewbox = svg_elem.getElementsByClassName("definition-scale")[0].getAttribute("viewBox");
   // find top of system
-  var svg_top = 0;
+  var svg_top = baseline;
 
-  if(!input)
-    input = document.getElementById(id_prefix+"treeinput").value;
-
-  var tree_g = svg_elem.getRootNode().getElementById("tree"+id_prefix);
+  var tree, tree_g = svg_elem.getRootNode().getElementById("tree"+id_prefix);
   var existing = tree_g ? true : false;
   if(existing)
     tree_g.parentNode.removeChild(tree_g);
 
-  var tree = JSON.parse(input);
-  var xlist = selected.concat(extraselected);
-  if(selected.length == 1){
-    if(selected[0].classList.contains("relation"))
-      xlist = relation_get_notes(selected[0]).map((n) => note_coords(get_by_id(document,id_in_svg(draw_context,n.getAttribute("xml:id"))))[0]);
-  }else if (selected[0].classList.contains("metarelation")){
-    //Somehow calculate x-coordinates for relations and metarelations
-  }else if (selected.length > 1){
-    xlist = xlist.map((n) => note_coords(n)[0]);
-  }else { //Test with all notes
-    xlist = Array.from(svg_elem.getElementsByClassName("note")).map((n) => note_coords(n)[0]);
-  } //TODO: once slicing is an option, try that.
+  if(!xmltree){
+    input = document.getElementById(id_prefix+"treeinput").value;
+    tree = JSON.parse(input);
+
+    // Extracting a list of x coordinates from a set of notes
+    var notelist = selected.concat(extraselected);
+    if(selected.length == 1){
+      if(selected[0].classList.contains("relation"))
+	notelist = relation_get_notes(selected[0]).map((n) => get_by_id(document,id_in_svg(draw_context,n.getAttribute("xml:id"))));
+    }else if (selected[0].classList.contains("metarelation")){
+      //Somehow calculate x-coordinates for relations and metarelations
+    }else if (selected.length > 1){
+      //Just use the selected notes
+    }else { //Test with all notes
+      notelist = Array.from(svg_elem.getElementsByClassName("note"));
+    } //TODO: once slicing is an option, try that.
+
+    var list = notelist.map((n) => [note_coords(n)[0],n]);
 
 
-  xlist.sort((a,b) => a - b);
+    list.sort((a,b) => a[0] - b[0]);
 
-  align_tree(tree,xlist, min_dist);
+    var leaves = gather_leaves(tree);
+    if(leaves.length != list.length){
+      console.log("Wrong length of list, expected "+leaves.length+" got "+list.length);
+      return;
+    }
+    for(i in leaves){
+      leaves[i].x = list[i][0];
+      leaves[i].note_id = list[i][1].getAttribute("xml:id");
+    }
+  }else{
+    tree = load_tree(xmltree);
+    find_x_tree(draw_context,tree);
+  }
+
+
+  align_tree(tree, min_dist);
 
 
   var tree_g = draw_node(tree);

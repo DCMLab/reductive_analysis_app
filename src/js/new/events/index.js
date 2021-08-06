ae8c8de32f3c0ce441bd0cf47ee9bd303872666d
Mehdi M <hi@mehdi.cc>
2021-08-06 20:26:05 +0200

    Debounce resize event

--- src/js/new/events/index.js
+++ src/js/new/events/index.js
@@ -1,20 +1,31 @@
 import { captureEvent } from './options'
+import debounceResize from './resize'
+
 class EventsManager {
   constructor(app) {
     this.app = app
+
     this.init()
   }
 
   init() {
+    window.addEventListener('resize', this.onResize.bind(this))
     document.addEventListener('click', this.onTap.bind(this), captureEvent)
 
     // add :hover support in iOS ¯\_(ツ)_/¯
     document.addEventListener('touchstart', () => {})
 
+    // fields
     document.addEventListener('change', this.onChange.bind(this), captureEvent)
 
     // core app events
-    document.addEventListener('undoredo', this.onUndoRedo.bind(this), captureEvent)
+    document.addEventListener('undoredo', this.onUndoRedo.bind(this))
+  }
+
+  onResize() {
+    debounceResize(() => {
+      this.app.ui?.onResize()
+    })
   }
 
   onTap(e) {
@@ -32,6 +43,4 @@ class EventsManager {
   }
 }
 
-export default function(app) {
-  return new EventsManager(app)
-}
+export default app => new EventsManager(app)

db21c691e1f6927d3624548a7c1f261f1405d21e
Petter Ericson <pettter@acc.umu.se>
2021-11-26 11:13:35 +0100

    This applies the copy_paste branch changes to the 'ui' branch, essentially doing the same work as #132, but for the new UI.

--- src/js/ui.js
+++ src/js/ui.js
@@ -34,6 +34,8 @@ import { draw_hierarchy_graph } from './visualizations'
 
 import { align_tree, draw_tree, load_tree, save_tree } from './trees'
 
+import { do_copy, do_paste }  from './copy_paste'
+
 import {
   button,
   checkbox,
@@ -431,6 +433,10 @@ export function handle_keypress(ev) {
     do_undo()
   } else if (ev.key == action_conf.redo) { // UNDO
     do_redo()
+  } else if (ev.key == action_conf.copy) { // COPY
+    do_copy()
+  } else if (ev.key == action_conf.paste) { // PASTE
+    do_paste()
   } else if (ev.key == action_conf.reduce_relations) { // Reduce relations
     do_reduce_pre(current_draw_context)
   } else if (ev.key == action_conf.show_hide_notation) { // Show/hide ties etc.

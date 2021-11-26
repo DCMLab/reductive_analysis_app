10b9c2112df9f1f3156cb3a9c0fed8048ec18a73
Mehdi M <hi@mehdi.cc>
2021-11-26 22:21:36 +0100

    Avoid useless computation in the CacheMap object

--- src/js/new/utils/cache.js
+++ src/js/new/utils/cache.js
@@ -8,18 +8,25 @@ export class CacheMap extends Map {
    *
    * @param {string} key
    * @param {*} value
-   * @returns CacheMap
+   * @returns {CacheMap}
    */
   add = (key, value) => this.has(key) ? this : this.set(key, value)
 
   /**
    * Adds a cache entry if the key is new in the cache, then returns the value.
    *
+   * The provided value can be of any type, and can also be a function that
+   * will only be executed if the key is new in the cache.
+   *
    * @param {string} key
-   * @param {*} value
+   * @param {*|function():*} value Value to cache or a function returning it.
    * @returns {*} Returns the `value` parameter.
    */
   remember = (key, value) => {
+    if (typeof value == 'function') {
+      value = this.has(key) ? this.get(key) : value()
+    }
+
     this.add(key, value)
     return value
   }

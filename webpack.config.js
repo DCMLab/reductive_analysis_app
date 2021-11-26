341fa9542f4b2165eda619cfbd4146a4fd4fde27
Mehdi M <hi@mehdi.cc>
2021-11-26 20:53:13 +0100

    Remove instruments from build processing
    It’s somehow compressed and broken by Webpack/Babel/Terser.

--- webpack.config.js
+++ webpack.config.js
@@ -223,7 +223,6 @@ configCSS = {
 configCSS.plugins.push(
   new CopyPlugin({ patterns: [
     { from: `${assets}/other-css/`, to: thePath('public/css/vendor') },
-    { from: `${assets}/instruments/`, to: thePath('public/instruments') },
     { from: `${assets}/fonts/`, to: thePath('public/fonts') },
     // { from: `${assets}/manifest/`, to: thePath('public') },
   ]}),

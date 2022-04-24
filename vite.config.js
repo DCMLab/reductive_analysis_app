77c81cbb66c25755b9ead1fae6cefcc433e0ffcc
yrammos <y@rammos.co>
2022-04-24 19:13:27 +0200

    Setup aforementioned plugin.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import eslintPlugin from 'vite-plugin-eslint'
+// license banners
+const path = require('path')
+const license = require('rollup-plugin-license')
 
 // env
 const env = require('dotenv').config().parsed
@@ -21,7 +24,7 @@ const esLintOptions = {
   cache: true, // cache is cleaned on `npm install`
   cacheStrategy: 'content',
   fix: env.ES_LINT_AUTOFIX == 'true',
-  formatter: env.ES_LINT_FORMATTER ?? 'stylish',
+  //  formatter: env.ES_LINT_FORMATTER ?? 'stylish',
 }
 
 export default defineConfig({
@@ -55,6 +58,22 @@ export default defineConfig({
   },
 
   plugins: [
+    license({
+      sourcemap: true,
+      banner: {
+        commentStyle: 'ignored',
+        content: {
+          file: path.join(__dirname, 'LICENSE'),
+        },
+      },
+
+      thirdParty: {
+        includePrivate: true,
+        output: {
+          file: path.join(__dirname, 'dist', 'dependencies.txt'),
+        },
+      },
+    }),
     ...(isProd ? [] : [eslintPlugin(esLintOptions)]),
   ],
 

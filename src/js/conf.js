6a390312a662e88d5d8022072cf689231756f07e
Petter Ericson <pettter@acc.umu.se>
2022-04-24 16:55:54 +0200

    Fix keybindings (#214)
    * Removes the use of the old deprecated type/meta/combo_keys.
    
    * Removes type/combo/meta_keys.
    
    * Removes the now unused type/meta/button_shades objects.

--- src/js/conf.js
+++ src/js/conf.js
@@ -1,9 +1,3 @@
-export const type_keys = {}
-export const type_shades = {}
-export const meta_keys = {}
-export const meta_shades = {}
-export const combo_keys = {}
-export const button_shades = {}
 
 // General action shortcuts.
 export const action_conf = {

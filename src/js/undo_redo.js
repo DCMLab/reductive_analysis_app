d3d77a84801b51b6a738512cc1b2a9a3bca6f67f
Yinan Zhou <yinan.a.zhou@gmail.com>
2025-03-13 14:47:34 -0400

    fix: update hierarchy tree after undo/redo

--- src/js/undo_redo.js
+++ src/js/undo_redo.js
@@ -71,6 +71,9 @@ export function do_undo() {
     sel.forEach(x => toggle_selected(document.getElementById(x.id), false))
     extra.forEach(x => toggle_selected(document.getElementById(x.id), true))
     redo_actions.push([what, [type, id], sel, extra])
+
+    // Update hierarchy tree if visible
+    window.relationTreeInstance?.updateIfVisible()
   } else if (what == 'delete relation') {
     var removed = elems
     removed.forEach(x => {
@@ -95,6 +98,8 @@ export function do_undo() {
     extra.forEach(x => toggle_selected(x, true))
     redo_actions.push([what, [], sel, extra])
 
+    // Update hierarchy tree if visible
+    window.relationTreeInstance?.updateIfVisible()
   } else if (what == 'change relation type') {
     var types = elems
     let type = elems[0][1]
@@ -112,6 +117,9 @@ export function do_undo() {
     sel.forEach(x => toggle_selected(x, false))
     extra.forEach(x => toggle_selected(x, true))
     redo_actions.push([what, type, sel, extra])
+
+    // Update hierarchy tree if visible
+    window.relationTreeInstance?.updateIfVisible()
   } else if (what == 'add note') {
     var [mei_elems, graphicals] = elems
     graphicals.forEach(x => x.parentNode.removeChild(x))

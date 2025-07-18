c03fff9f4662c8c5d11a05e5c7ffd32ad933fa0e
Robin Nicole <crowerade@protonmail.com>
2025-07-18 14:34:54 +0200

    fix: clash between unrelated notes and reduction

--- src/js/modules/UI/Score/utils.js
+++ src/js/modules/UI/Score/utils.js
@@ -15,13 +15,13 @@ export function hide_orphan_notes() {
   ids.forEach(i => {
     var ii = i.replace(/([a-z]\d-)+/, '') // Replace layer or view prefixes.
     if (!gn_ids.includes(`#gn-${ii}`)) {
-      document.getElementById(i).classList.add('hidden')
+      document.getElementById(i).classList.add('hidden-orphan')
     }
   })
 }
 
 export function show_all_notes() {
-  Array.from(document.querySelectorAll('g.note')).forEach(e => e.classList.remove('hidden'))
+  Array.from(document.querySelectorAll('g.note')).forEach(e => e.classList.remove('hidden-orphan'))
 }
 
 export function set_non_note_visibility(hidden) {

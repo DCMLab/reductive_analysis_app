b1ea24be3bd696baa422f6a46b716178d916430f
Robin Nicole <crowerade@protonmail.com>
2025-07-28 10:22:10 +0200

    fix: parent deletion would overdelete some edges
    I commented out code that seems overly complicated, or maybe obsolete

--- src/js/action/delete.js
+++ src/js/action/delete.js
@@ -63,6 +63,7 @@ function delete_relation(elem) {
         const sourceElement = get_by_id(mei, fromId)
 
         // If the source is a metarelation, add the target as a child ID
+        // TODO: Why?
         if (sourceElement && sourceElement.getAttribute('type') === 'metarelation') {
           childIds.add(toId)
         }
@@ -86,14 +87,14 @@ function delete_relation(elem) {
 
   // After deletion, check if any children need their connection circles removed
   // Store the returned connection circle data for undo
-  if (childIds.size > 0) {
-    const connectionCircleData = updateConnectionCircles(Array.from(childIds))
+  // if (childIds.size > 0) {
+  //   const connectionCircleData = updateConnectionCircles(Array.from(childIds))
 
-    // Add connection circle data to the action_removed array if any was removed
-    if (connectionCircleData.length > 0) {
-      action_removed.push(['connection_circle_data', connectionCircleData])
-    }
-  }
+  //   // Add connection circle data to the action_removed array if any was removed
+  //   if (connectionCircleData.length > 0) {
+  //     action_removed.push(['connection_circle_data', connectionCircleData])
+  //   }
+  // }
 
   // Adjust SVG dimensions after deletion
   draw_contexts.forEach(draw_context => {

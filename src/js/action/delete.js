555b1a5f0133a6641b28bdb434eeb8f4bdfea956
Robin Nicole <crowerade@protonmail.com>
2025-08-15 14:33:10 +0200

    fix: relation deletion would not delete arcs in mei

--- src/js/action/delete.js
+++ src/js/action/delete.js
@@ -32,8 +32,8 @@ function delete_relation(elem) {
   // Find all arcs related to this element
   var arcs =
     Array.from(mei.getElementsByTagName('arc')).filter((arc) => {
-      return (arc.getAttribute('from') == '#' + elem.id.slice(1) ||
-              arc.getAttribute('to') == '#' + elem.id.slice(1))
+      return (arc.getAttribute('from') == '#' + elem.id ||
+              arc.getAttribute('to') == '#' + elem.id)
     })
   // Find meta-relations associated with this relation
   const result = find_all_parent_relations(elem, mei, draw_contexts, svg_hes)

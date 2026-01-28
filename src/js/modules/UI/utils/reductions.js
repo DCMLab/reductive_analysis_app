61040a3305240e51dcb331062c71cb1f917884c4
yrammos <y@rammos.co>
2026-01-28 17:00:59 +0100

    Boilerplate for musical exception handling.

--- src/js/modules/UI/utils/reductions.js
+++ src/js/modules/UI/utils/reductions.js
@@ -25,15 +25,27 @@ export async function reduce() {
         body: new XMLSerializer().serializeToString(mei)
       })
       const verdict = await response.json()
-      console.log('analysis = ', verdict)
+
+      // Case 1: We received a reductive analysis.
       if (Array.isArray(verdict) && verdict.length === 2 && Array.isArray(verdict[0]) && Array.isArray(verdict[1])) {
         fetched_note_diffs = verdict[0].reverse()
         fetched_relation_diffs = verdict[1].reverse()
         console.log(fetched_note_diffs)
         console.log(fetched_relation_diffs)
       }
+
+      // Case 2: We received a graph cycle.
+      if (Array.isArray(verdict) && verdict.length === 2 && typeof (verdict[0]) == 'string' && verdict[0] == 'GraphCycleError' && Array.isArray(verdict[1])) {
+        alert('Graph cycle.')
+      }
+
+      // Case 3: We received another exception.
+      if (Array.isArray(verdict) && verdict.length === 2 && typeof (verdict[0]) == 'string' && verdict[0] == 'Error' && typeof (verdict[1]) == 'string') {
+        alert(`${verdict[1]}`)
+      }
+
     } catch (error) {
-      alert('Failed to fetch reductive stages:', error)
+      alert('Error:', error)
       return
     }
 
